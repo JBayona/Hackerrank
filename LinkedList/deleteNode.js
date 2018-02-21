316ce9060a93e9c15968c287a626e6a825302d34
JBayona <jordavids_22@hotmail.com>
2018-02-20 22:22:14 -0800

    Delete Node in specific position

--- LinkedList/deleteNode.js
+++ LinkedList/deleteNode.js
@@ -1,4 +1,5 @@
 /*
+
 You’re given the pointer to the head node of a linked list and the position of a node to delete. Delete the node at the given position and return the head node. A position of 0 indicates head, a position of 1 indicates one node away from the head and so on. The list may become empty after you delete the node.
 
 Input Format 
