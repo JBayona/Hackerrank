c02d4bee716c10be6429377df358fefcefbe353a
JBayona <jordavids_22@hotmail.com>
2018-04-01 14:13:53 -0700

    Reverse LinkedList.

--- LinkedList/reverseLinkedList.js
+++ LinkedList/reverseLinkedList.js
@@ -1,4 +1,5 @@
 /*
+
 You’re given the pointer to the head node of a linked list. Change the next pointers of the nodes so that their order is reversed. The head pointer given may be null meaning that the initial list is empty.
 
 Input Format 
