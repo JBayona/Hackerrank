51da2c4e25ef07a4f28d1894c80d98e97733509c
JBayona <jordavids_22@hotmail.com>
2018-04-11 23:20:39 -0700

    Insert Head.

--- LinkedList/insertHead.js
+++ LinkedList/insertHead.js
@@ -1,5 +1,4 @@
 /*
-
 You’re given the pointer to the head node of a linked list and an integer to add to the list. Create a new node with the given integer, insert this node at the head of the linked list and return the new head node. The head pointer given may be null meaning that the initial list is empty.
 
 Input Format 
