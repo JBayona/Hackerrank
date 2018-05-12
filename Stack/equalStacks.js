2b9aad844327fa6cfb1b3f6acc189bbfe6aa5a24
JBayona <jordavids_22@hotmail.com>
2018-05-12 11:38:03 -0700

    Equal Stack.

--- Stack/equalStacks.js
+++ Stack/equalStacks.js
@@ -1,5 +1,4 @@
 /*
-
 You have three stacks of cylinders where each cylinder has the same diameter, but they may vary in height.
 You can change the height of a stack by removing and discarding its topmost cylinder any number of times.
 
@@ -18,7 +17,6 @@ The third line contains  space-separated integers describing the cylinder height
 The fourth line contains  space-separated integers describing the cylinder heights in stack .
 
 https://www.hackerrank.com/challenges/equal-stacks/problem
-
 */
 
 function equalStacks(h1, h2, h3) {
