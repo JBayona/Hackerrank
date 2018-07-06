76a9db2f89fee8a937b1d93d19ff86bc46387adf
JBayona <jordavids_22@hotmail.com>
2018-07-05 20:02:26 -0700

    Preorder java.

--- Trees/preOrder.java
+++ Trees/preOrder.java
@@ -1,5 +1,4 @@
 /*
-
 Complete the preOrder function in your editor below, which has  parameter: a 
 pointer to the root of a binary tree. It must print the values in the tree's 
 preorder traversal as a single line of space-separated values.
