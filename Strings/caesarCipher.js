ff30f7f5097f977eb4cb02ef8370942ffaf3d6b3
JBayona <jordavids_22@hotmail.com>
2018-11-24 22:05:16 -0800

    Caeser Cipher.

--- Strings/caesarCipher.js
+++ Strings/caesarCipher.js
@@ -1,5 +1,4 @@
 /*
-
 Julius Caesar protected his confidential information by encrypting it in a cipher. Caesar's cipher rotated every letter in a string by a fixed number, , making it unreadable by his enemies. Given a string, , and a number, , encrypt  and print the resulting string.
 
 Note: The cipher only encrypts letters; symbols, such as -, remain unencrypted.
